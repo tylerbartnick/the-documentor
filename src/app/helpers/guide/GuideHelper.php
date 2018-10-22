acd7fd709113bddad39669ebdc3b1b349215e99e
tylerbartnick <bartnickt@gmail.com>
2018-10-21 22:22:39 -0400

    Updated storage of guide content

--- src/app/helpers/guide/GuideHelper.php
+++ src/app/helpers/guide/GuideHelper.php
@@ -8,30 +8,6 @@ class GuideHelper
 {
     public static function sanitizeContent(string $content)
     {
-        $dom = new \DOMDocument();
-        $dom->loadHTML($content);
-
-        $scriptTags = $dom->getElementsByTagName('script');
-        $linkTags   = $dom->getElementsByTagName('link');
-        $iframeTags = $dom->getElementsByTagName('iframe');
-        $styleTags  = $dom->getElementsByTagName('style');
-
-        foreach ($scriptTags as $tag) {
-            $tag->parentNode->removeChild($tag);
-        }
-
-        foreach ($linkTags as $tag) {
-            $tag->parentNode->removeChild($tag);
-        }
-
-        foreach ($iframeTags as $tag) {
-            $tag->parentNode->removeChild($tag);
-        }
-
-        foreach ($styleTags as $tag) {
-            $tag->parentNode->removeChild($tag);
-        }
-
-        return $dom->saveHTML();
+        return strip_tags($content);
     }
 }
